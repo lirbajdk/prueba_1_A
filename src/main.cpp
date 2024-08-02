32bedf57f8667caa5de512a28344585d33acded5
lirbajdk <lirbajdk@gmail.com>
2024-08-02 14:04:43 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,39 @@
 #include <Arduino.h>
 
-int main(void){
+void prender_leds(uint8_t cual);
 
+uint8_t sumar(uint8_t valA, uint8_t valB);
 
+int main(void)
+{
 
-/*escribo algo*/
+    prender_leds(5);
 
-/*escribo otra cosa*/
+    uint8_t resu = sumar(5,90);
 
+    /*escribo algo*/
+
+    /*escribo otra cosa*/
+}
+
+//------------------------------------------------------
+//------------------------------------------------------
+uint8_t sumar(uint8_t valA, uint8_t valB)
+{
+    return valA + valB;
+}
+
+//------------------------------------------------------
+//------------------------------------------------------
+void prender_leds(uint8_t cual)
+{
+
+    switch (cual)
+    {
+    }
+
+    PORTD |= (1 << PD4);
+    PORTD |= (1 << PD5);
+    PORTD |= (1 << PD6);
+    PORTD |= (1 << PD7);
 }
